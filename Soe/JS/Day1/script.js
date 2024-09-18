816b6785a230024ffbcb242d62d0aca418660b31
maythinn <90564038+developer-maythinn@users.noreply.github.com>
2024-09-18 09:34:45 +0630

    js

---
+++ Soe/JS/Day1/script.js
@@ -0,0 +1,32 @@
+
+// document.getElementById("demo").innerHTML = "<h1>Hello World</h1>";
+// document.getElementById("demo2").innerText = "<h1>JavaScript</h1>";
+
+// console.log(document.getElementById("demo"));
+
+// var username = "John";
+// let age = 20;
+// age = 30;
+// const city = "Ygn";
+// // city = "Mdy"; // type error
+// console.log("Username", username, age, city);
+
+// let city = "Ygn";
+// let city2 = city;
+// console.log(city2);
+
+// let username = "John";
+// let Username = "Jobs";
+
+// var username = "John";
+
+// if (true) {
+//   const username = "Jobs";
+//   console.log("let", username);
+// }
+// console.log("var", username);
+
+"use strict";
+// num = 20;
+let num = 20;
+console.log(num);
