0e2ae02ccd4aa9351607614f7a228a52255a55c5
maythinn <90564038+developer-maythinn@users.noreply.github.com>
2024-09-19 11:56:55 +0630

    js

---
+++ Soe/JS/Day2/data-types.js
@@ -0,0 +1,38 @@
+// let num = 5;
+// console.log(typeof num);
+// console.log("not a number" + 2);
+// // console.log(1 / 0);
+// console.log("not a number" / 2);
+
+// let username = "John";
+// let message = `Hello`;
+// // let result = message + " " + username;
+// let result = `${message} ${username}. How are you?`;
+// console.log(typeof username, message);
+// console.log(result);
+
+// let isGreater = 4 > 1;
+// console.log(typeof isGreater);
+
+// let num;
+// let message = null;
+// console.log(typeof num);
+// console.log(typeof message);
+// console.log(num == message); // value
+// console.log(num === message); // value, type
+
+// const fruits = ["apple", "orange", "mango"];
+// console.log(fruits[0]);
+// console.log(fruits[1]);
+// console.log(fruits[2]);
+// console.log(fruits[3]) // undefined
+
+// const person = {
+//   name: "John",
+//   age: 20,
+// };
+// console.log(person.name, person.age);
+
+// alert("Hello");
+// prompt("How are you?");
+// confirm("Are you a boss?");
---
+++ Soe/JS/Day2/operators.js
@@ -0,0 +1,54 @@
+// JavaScript Arithmetic Operators
+// let num1 = 10;
+// let num2 = 2;
+// console.log(num1 + num2);
+// console.log(num1 - num2);
+// console.log(num1 * num2);
+// console.log(num1 ** num2);
+// console.log(num1 / num2);
+// console.log(num1 % num2);
+// num1++;
+// num1++;
+// num1--;
+// console.log(num1);
+// console.log(num1 - num2);
+
+// JavaScript Assignment Operators
+
+// let num1 = 2;
+// // num1 += 4; // 2 + 4
+// // num1 -= 1
+// console.log(num1)
+
+// JavaScript String Operators
+// let message = "5" + 5;
+// let message = 5 + 5;
+
+// let message = "Hello ";
+// message += "John";
+// console.log(message);
+
+// JavaScript Comparison Operators
+
+// console.log(10 == "10"); // true
+// console.log(10 === "10"); // false
+// console.log(10 != "10"); // false
+// console.log(10 !== "10"); // true
+// console.log(10 > 2);
+// console.log(2 < 10);
+// console.log(2 >= 2);
+
+// // ternary operator
+// let age = 18;
+// let result = age >= 18 ? "Adult" : "Child";
+// console.log(result);
+
+// JavaScript Logical Operators
+let username = "John";
+console.log(username === "John");
+console.log(typeof username === "string");
+// console.log(username === "John" && typeof username === "number");
+
+// console.log(username === "John" || typeof username === "number");
+let result = !(username === "John");
+console.log(result);
